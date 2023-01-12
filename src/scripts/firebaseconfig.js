762bbcdc7cd30522691e78158b48ae7573e4e949
Michael Vargas <Michael.Vargas0290@gmail.com>
2023-01-12 12:19:23 -0600

    Add Firebase to project.

---
+++ src/scripts/firebaseconfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDYGPZYvJWJ8RPatOA-nueHCWbfY9Y-6QI",
+  authDomain: "striking-web-designs-site.firebaseapp.com",
+  projectId: "striking-web-designs-site",
+  storageBucket: "striking-web-designs-site.appspot.com",
+  messagingSenderId: "434421987585",
+  appId: "1:434421987585:web:d0eb888f285295a0c6c300",
+  measurementId: "G-QG3831Q1YV",
+};
+
+initializeApp(firebaseConfig);
